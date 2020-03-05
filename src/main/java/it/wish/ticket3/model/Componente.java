e433710f0137713bbc1d92678a0375778c0d30cf
bastianelli <michele.bastio@gmail.com>
2020-03-05 17:16:09 +0100

    aggiunto model Componente

---
+++ src/main/java/it/wish/ticket3/model/Componente.java
@@ -0,0 +1,50 @@
+package it.wish.ticket3.model;
+
+public class Componente {
+	private String descrizione;
+	private Double costoUnitario;
+	private Integer quantita;
+	private Double costoTot;
+	
+	public Componente () {}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public Double getCostoUnitario() {
+		return costoUnitario;
+	}
+
+	public void setCostoUnitario(Double costoUnitario) {
+		this.costoUnitario = costoUnitario;
+	}
+
+	public Integer getQuantita() {
+		return quantita;
+	}
+
+	public void setQuantita(Integer quantita) {
+		this.quantita = quantita;
+	}
+
+	public Double getCostoTot() {
+		return costoTot;
+	}
+
+	public void setCostoTot(Double costoTot) {
+		this.costoTot = costoTot;
+	}
+
+	@Override
+	public String toString() {
+		return "Componente [descrizione=" + descrizione + ", costoUnitario=" + costoUnitario + ", quantita=" + quantita
+				+ ", costoTot=" + costoTot + "]";
+	}
+	
+	
+}
